52854d5aa38e2a05137665f3b7a80244b2aa59e2
martin <martin.labat92@gmail.com>
2023-03-20 19:37:34 +0100

    feat: install clip-ViT-B-32 model
    Signed-off-by: martin <martin.labat92@gmail.com>

--- root/defaults/install.py
+++ root/defaults/install.py
@@ -1,6 +1,8 @@
 from transformers import pipeline
+from sentence_transformers import SentenceTransformer
 
 # This script installs the models in the TRANSFORMERS_CACHE folder
 
-classifier = pipeline(task="image-classification", model="microsoft/resnet-50")
-detector = pipeline(task="object-detection", model="hustvl/yolos-tiny")
+classification_model = pipeline(task="image-classification", model="microsoft/resnet-50")
+object_model = pipeline(task="object-detection", model="hustvl/yolos-tiny")
+clip_model = SentenceTransformer("clip-ViT-B-32")
