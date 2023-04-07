62edc0fd3bea26fa9de48459081f2cf402e22c84
martin <martin.labat92@gmail.com>
2023-04-07 20:00:58 +0200

    feat: use context manager
    Signed-off-by: martin <martin.labat92@gmail.com>

--- root/defaults/migrate.py
+++ root/defaults/migrate.py
@@ -11,29 +11,30 @@ database = os.environ.get('DB_DATABASE_NAME', '')
 
 # Connect to the database
 try:
-    conn = psycopg2.connect(
+    with psycopg2.connect(
         host=host,
         port=port,
         user=username,
         password=password,
         dbname=database
-    )
+    ) as conn:
+        # Open a cursor to perform database operations
+        with conn.cursor() as cur:
+            # Define a SQL query to update the rows in the assets table
+            sql = """
+                UPDATE assets
+                SET "originalPath" = REPLACE("originalPath", 'upload/', '/photos/'),
+                    "resizePath" = REPLACE("resizePath", 'upload/', '/photos/'),
+                    "webpPath" = REPLACE("webpPath", 'upload/', '/photos/'),
+                    "encodedVideoPath" = REPLACE("encodedVideoPath", 'upload/', '/photos/')
+                WHERE "originalPath" LIKE 'upload/%';
+            """
 
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
+            # Execute the SQL query
+            cur.execute(sql)
 
-    # Define a SQL query to update the rows in the assets table
-    sql = "UPDATE assets SET \"originalPath\" = REPLACE(\"originalPath\", 'upload/', '/photos/'), \"resizePath\" = REPLACE(\"resizePath\", 'upload/', '/photos/'), \"webpPath\" = REPLACE(\"webpPath\", 'upload/', '/photos/'), \"encodedVideoPath\" = REPLACE(\"encodedVideoPath\", 'upload/', '/photos/') WHERE \"originalPath\" LIKE 'upload/%';"
-
-    # Execute the SQL query
-    cur.execute(sql)
-
-    # Commit the changes to the database
-    conn.commit()
-
-    # Close the cursor and database connection
-    cur.close()
-    conn.close()
+            # Commit the changes to the database
+            conn.commit()
 
 
 except psycopg2.Error as e:
