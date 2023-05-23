2f5371971bfde8d8892c755079bc112cfb06dd17
martabal <74269598+martabal@users.noreply.github.com>
2023-05-23 15:22:13 +0200

    fix: change env to enable cuda acceleration
    Signed-off-by: martabal <74269598+martabal@users.noreply.github.com>

--- root/defaults/main.py
+++ root/defaults/main.py
@@ -35,7 +35,7 @@ _model_cache = {}
 
 app = FastAPI()
 
-cuda_acceleration = os.getenv("ML_CUDA") == "true"
+cuda_acceleration = os.getenv("CUDA_ACCELERATION") == "true"
 provider = ["CUDAExecutionProvider", "CPUExecutionProvider"] if cuda_acceleration else ["CPUExecutionProvider"]
 device_pipeline = 0 if cuda_acceleration else -1
 device_sentence_transformer = "cuda" if cuda_acceleration else "cpu"
